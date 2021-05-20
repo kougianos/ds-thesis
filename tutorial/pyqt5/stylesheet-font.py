b3676e10857f42a9ff1d80fc02c69ff86d11cd59
Nikos Kougianos <kougi_jr@hotmail.com>
2021-05-20 22:56:27 +0300

    Create stylesheet-font.py

---
+++ tutorial/pyqt5/stylesheet-font.py
@@ -0,0 +1,33 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, )
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+        self.setMinimumHeight(600)
+        self.setMinimumWidth(600)
+
+        widget = QLabel("Hello")
+        widget.setStyleSheet("margin:auto 25px auto; padding-bottom: 50%; border: 2px solid red; border-radius: 50px")
+        font = widget.font()
+        font.setPointSize(30)
+        font.setBold(True)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
+
+        self.setCentralWidget(widget)
+
+
+app = QApplication(sys.argv)
+app.setStyle('Fusion')
+w = MainWindow()
+w.show()
+app.exec()
