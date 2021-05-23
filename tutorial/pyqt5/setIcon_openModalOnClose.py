1369113d4f6524232f1e28af12a0d29ad7b82e67
Nikos Kougianos <kougi_jr@hotmail.com>
2021-05-24 00:06:45 +0300

    Update setIcon_openModalOnClose.py

--- tutorial/pyqt5/setIcon_openModalOnClose.py
+++ tutorial/pyqt5/setIcon_openModalOnClose.py
@@ -1,9 +1,7 @@
-from PyQt5 import QtWidgets, QtGui
-import sys
 import ctypes
+import sys
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction
+from PyQt5 import QtWidgets, QtGui
 
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
 
@@ -30,36 +28,11 @@ class MainWindow(QtWidgets.QMainWindow):
         if reply == QtWidgets.QMessageBox.Yes:
             event.accept()
         else:
-            self.tray_icon.show()
-            self.hide()
             event.ignore()
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon('neural.png'))
-    app.setQuitOnLastWindowClosed(False)
-    w = MainWindow()
-
-    # Adding an icon
-    icon = QIcon("neural.png")
-
-    # Adding item on the menu bar
-    tray = QSystemTrayIcon()
-    tray.setIcon(icon)
-    tray.setVisible(True)
-
-    # Creating the options
-    menu = QMenu()
-    option1 = QAction("My App")
-    menu.addAction(option1)
-
-    # To quit the app
-    quit = QAction("Quit")
-    quit.triggered.connect(app.quit)
-    menu.addAction(quit)
-
-    # Adding options to the System Tray
-    tray.setContextMenu(menu)
+    window = MainWindow()
 
     app.exec()
