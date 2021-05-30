6161e26895e6cffb2e253d36b456b9c228001448
Nikos Kougianos <kougi_jr@hotmail.com>
2021-05-30 23:30:22 +0300

    Update openfilename.py

--- tutorial/openfilename.py
+++ tutorial/openfilename.py
@@ -2,12 +2,11 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import os
 
-# Current directory the script is placed
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-tk.Tk().withdraw()
+def open_file_name():
+    # Current directory the script is placed
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    tk.Tk().withdraw()
 
-fn = askopenfilename(initialdir=dir_path)
-print(fn)
-f = open(fn, 'r')
-print(f.read())
+    filename = askopenfilename(initialdir=dir_path)
+    return filename
