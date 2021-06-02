7605899c3931e71207e9bf7a5fca09e702bc149d
Nikos Kougianos <kougi_jr@hotmail.com>
2021-06-02 10:59:44 +0300

    Update import_another_pythonfile.py

--- tutorial/import_another_pythonfile.py
+++ tutorial/import_another_pythonfile.py
@@ -1,8 +1,9 @@
 import sys
 
-# Disable __pycache__ folder creation
-sys.dont_write_bytecode = True
-sys.path.append('./test_folder')
-from test_folder import printHello as Ph
+sys.dont_write_bytecode = True              # Disable __pycache__ folder creation
+sys.path.append('./test_folder')            # Import test folder in system path
+from test_folder import printHello as Ph    # Different directory
+from open_file_name import open_file_name   # Same directory
 
 Ph.hello_world()
+open_file_name()
